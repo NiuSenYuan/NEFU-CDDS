01251e9c3929aae01024a88a2419da59f8e67d53
NiuSenYuan <128779006+NiuSenYuan@users.noreply.github.com>
2023-12-11 17:07:58 +0800

    Update problem 2.cpp

--- Problem 2/problem 2.cpp
+++ Problem 2/problem 2.cpp
@@ -7,19 +7,19 @@ using namespace std;
 const int INF=0x3f3f3f3f;
 typedef struct message
 {
-	int num;       /??? 
-	string name;   /??? 
-	string pro;    /??? 
+	int num;       //景点代码 
+	string name;   //景点名称 
+	string pro;    //简介 
 }Ciceroni;       
 struct Graph{
 	int vexnum;
 	int arcnum;
 	int arcs[MAXNUM][MAXNUM];
-	int answer[MAXNUM][MAXNUM];
-	Ciceroni ScenicSpot[MAXNUM];
-	bool vis[MAXNUM];
-	int path[MAXNUM];
-	/???? 
+	int answer[MAXNUM][MAXNUM];  //最短路径 
+	Ciceroni ScenicSpot[MAXNUM]; //景点信息 
+	bool vis[MAXNUM];  //标记景点 
+	int path[MAXNUM];  //纪录路径 
+	//初始化 
 	Graph()
 	{
 		vexnum=0;
@@ -27,7 +27,7 @@ struct Graph{
 		memset(arcs,INF,sizeof arcs);
 		memset(vis,false,sizeof vis);
 	}
-	/??????????? 
+	//得到景点编号 
 	int GetNum(const string& Name)
 	{
 		for(int i=1;i<=this->vexnum;i++)
@@ -39,12 +39,12 @@ struct Graph{
 		}
 		return 0;
 	}
- 	/???? 
+ 	//添加道路 
 	void AddEdge(int vex1,int vex2,int dis)
 	{
 		arcs[vex1][vex2]=arcs[vex2][vex1]=dis;
 	}
-	/???
+	//删除景点 
 	void DeleteSpot(int id)
 	{
 		for(int i=id;i<=vexnum-1;i++)
@@ -61,14 +61,14 @@ struct Graph{
 		}
 		Floyd();
 	} 
-	/?????
+	//更新景点 
 	void UpdateSpot(int id)
 	{
 		string data;
 		cin>>data;
 		ScenicSpot[id].pro=data;
 	} 
-	/????????????? 
+	//弗洛伊德算法求景点之间的最短路 
 	void Floyd()
 	{
 		for(int k=1;k<=this->vexnum;k++)
@@ -82,53 +82,53 @@ struct Graph{
 			}
 		}
 	}
-	/?????????? 
+	//打印景点之间的最短路 
 	void PrintMinDis()
 	{
-		cout<<"-------???????????-------"<<endl;
+		cout<<"-------各景点之间最短路径如下-------"<<endl;
 		for(int i=1;i<=vexnum;i++)
 		{
 			for(int j=i+1;j<=vexnum;j++)
 			{
-				cout<<ScenicSpot[i].name<<"?"<<ScenicSpot[j].name<<"??????:";
+				cout<<ScenicSpot[i].name<<"到"<<ScenicSpot[j].name<<"最短距离为:";
 				cout<<arcs[i][j]<<endl;
 			}
 		}
 	}
-	/???????????????
+	//得到景点之间的所有路径 
 	void DFS(int spot1,int spot2,int order)
 	{
 		path[order]=spot1;
-		vis[spot1]=true;
 		if(spot1==spot2)
 		{
 			for(int i=1;i<order;i++)
 			{
-				cout<<this->ScenicSpot[i].name<<"->";
+				cout<<this->ScenicSpot[path[i]].name<<"->";
 			}
-			cout<<this->ScenicSpot[order].name<<endl;
+			cout<<this->ScenicSpot[path[order]].name<<endl;
 			return;
 		}
 		for(int i=1;i<=this->vexnum;i++)
 		{
-			if(!vis[i]&&this->arcs[spot1][i]<INF)
+			if(!vis[i]&&this->arcs[spot1][i]<10000)
 			{
+				vis[i]=true;
 				DFS(i,spot2,order+1);
+				vis[i]=false;
 			}
 		}
-		vis[spot1]=false;
 	} 
 };
 struct Graph G;
 int main()
 {
-	// ??????????????? 
+	//输入景点数和道路数 
     cin>>G.vexnum>>G.arcnum;
     for(int i=1;i<=G.vexnum;i++)
     {
     	cin>>G.ScenicSpot[i].num>>G.ScenicSpot[i].name>>G.ScenicSpot[i].pro;
 	}
-	cout<<"?????????????????!"<<endl; 
+	cout<<"输入景点名称和景点之间的距离："<<endl; 
 	for(int i=1;i<=G.arcnum;i++)
 	{
 		string spot1,spot2;
@@ -144,7 +144,7 @@ int main()
 			}
 			else
 			{
-				cout<<"??????,?????!"<<endl;
+				cout<<"景点名称有误"<<endl;
 			}
 		}			
 	}
@@ -161,22 +161,30 @@ int main()
     return 0;
 }
 /*
-input
-6 10
-1 ???  ????? 
-2 ??? ????? 
-3 ??? ????? 
-4 ??? ????? 
-5 ???? ????? 
-6 ??? ????? 
-??? ??? 100
-??? ??? 50
-??? ??? 400
-??? ??? 100
-??? ???? 50
-??? ???? 200
-??? ??? 800
-??? ??? 300
-??? ??? 100
-??? ??? 250
-*/
+10 15
+1 行政楼 办公 
+2 食堂 吃饭 
+3 赛博楼 信息分院办公室所在地
+4 求是楼 实验楼计算机中心
+5 格致楼 法学管理学院
+6 工程实习中心 金工实习
+7 仰仪楼 机电计测分院
+8 体育馆 旁边有篮球场、足球场、还有网球场
+9 一号教学楼 主要以阶梯教室为主
+10 二号教学楼 小教室为多
+行政楼 食堂 300
+行政楼 仰仪楼 200
+体育馆 一号教学楼 150
+二号教学楼 仰仪楼 350
+一号教学楼 二号教学楼 400
+工程实习中心 格致楼 500
+行政楼 工程实习中心 200
+求是楼 仰仪楼 100
+体育馆 工程实习中心 150
+食堂 体育馆 100
+体育馆 赛博楼 450
+格致楼 二号教学楼 50
+求是楼 赛博楼 150
+一号教学楼 食堂 300
+二号教学楼 工程实习中心  70
+*/ 
